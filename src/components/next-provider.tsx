8e09836e04989f3439cd3e03b65af4976ab40008
arefmaghdam <arefmaghdam@gmail.com>
2024-01-15 23:49:43 +0330

    feat: implement next provider and ts provider components for routing

---
+++ src/components/next-provider.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import Link from "next/link";
+
+const NextProvider: FC = () => {
+  return (
+    <div>
+      <Link href="/components/nextjs/setting-up">setting-up</Link>
+    </div>
+  );
+};
+
+export default NextProvider;
---
+++ src/components/ts-provider.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import Link from "next/link";
+
+const TSProvider: FC = () => {
+  return (
+    <div>
+      <Link href="/javascript/introduction">introduction</Link>
+    </div>
+  );
+};
+
+export default TSProvider;
