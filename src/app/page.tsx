0175d123b0a861a3da747192888ca7fdd4dc6201
arefmaghdam <arefmaghdam@gmail.com>
2024-01-15 23:48:08 +0330

    feat: design home page of app with css module styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <h1>Hello from next 13</h1>;
+import NextProvider from "@/components/next-provider";
+import TSProvider from "@/components/ts-provider";
+
+import styles from "./page.module.css";
+import { ReactElement } from "react";
+
+export default function Home(): ReactElement {
+  return (
+    <>
+      <div className={styles.homeContainer}>
+        <div className={styles.homeItems}>
+          <h1>Next.JS 13</h1>
+          <NextProvider />
+        </div>
+        <div className={styles.homeItems}>
+          <h1>Typescript</h1>
+          <TSProvider />
+        </div>
+      </div>
+    </>
+  );
 }
